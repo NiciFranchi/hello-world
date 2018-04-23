83879ff7b4b5d1b78c01af83f453024bf68b77a1
NiciFranchi <38140796+NiciFranchi@users.noreply.github.com>
2018-04-23 14:10:32 +0100

    correct filename

--- chitchat.java
+++ chitchat.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {   # What's this
+        System.out.print("Hello, out there!");
         Scanner userInputScanner = new Scanner(System.in);
         System.out.print("What's your name? ");
         String userInputName = userInputScanner.nextLine();
